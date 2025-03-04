18dd932031485d21d2fa68f9e0afac1082581463
LeRaphouu <raphael.bonnet@ynov.com>
2025-03-04 17:26:11 +0100

    Scanners.java fini avec EvenOrOdd

--- src/fr/ynov/java/easy/Scanners.java
+++ src/fr/ynov/java/easy/Scanners.java
@@ -10,5 +10,18 @@ public class Scanners {
         String name = String.valueOf(sc.nextLine());
 
         System.out.println(name);
+
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("Choose Numbers : ");
+
+        int choice1 = sc1.nextInt();
+        System.out.println(choice1);
+
+        Scanner sc2 = new Scanner(System.in);
+        if (choice1 % 2 == 0) {
+            System.out.println("Even Numbers");
+        } else {
+            System.out.println("Odd Numbers");
+        }
     }
 }
