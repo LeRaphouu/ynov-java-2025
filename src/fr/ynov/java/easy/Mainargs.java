7d835b0350a3d7d6ea9b9e59de4310897a003abd
LeRaphouu <raphael.bonnet@ynov.com>
2025-03-04 17:35:05 +0100

    Mainargs.java added

---
+++ src/fr/ynov/java/easy/Mainargs.java
@@ -0,0 +1,20 @@
+package fr.ynov.java.easy;
+
+import java.util.Scanner;
+
+public class Mainargs {
+    public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter Number : ");
+    int choice = sc.nextInt();
+
+    Scanner operator = new Scanner(System.in);
+
+    Scanner sc1 = new Scanner(System.in);
+    System.out.println("Enter second Number : ");
+    int choice2 = sc1.nextInt();
+
+
+    }
+}
