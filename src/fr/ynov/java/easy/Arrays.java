985cc3675d7de38106b819bb2d903953bcb5b346
LeRaphouu <raphael.bonnet@ynov.com>
2025-03-04 17:06:31 +0100

    Arrays.java ended

--- src/fr/ynov/java/easy/Arrays.java
+++ src/fr/ynov/java/easy/Arrays.java
@@ -1,4 +1,22 @@
 package fr.ynov.java.easy;
 
 public class Arrays {
+    public static void main(String[] args) {
+
+    char[] f = new char[10];
+        f[0] = 'H';
+        f[1] = 'e';
+        f[2] = 'l';
+        f[3] = 'l';
+        f[4] = 'o';
+        f[5] = ' ';
+        f[6] = 'W';
+        f[7] = 'o';
+        f[8] = 'r';
+        f[9] = 'd';
+
+        for (int i = 0; i < f.length; i++) {
+            System.out.print(f[i]);
+        }
+    }
 }
