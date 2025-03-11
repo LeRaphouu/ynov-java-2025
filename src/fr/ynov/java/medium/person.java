bcb46ee65f0f801057a7117a64b75847d97fffc1
LeRaphouu <raphael.bonnet@ynov.com>
2025-03-11 15:15:11 +0100

    person.java birthdate

--- src/fr/ynov/java/medium/person.java
+++ src/fr/ynov/java/medium/person.java
@@ -1,21 +1,41 @@
 package fr.ynov.java.medium;
 
- enum Nationality {
+import java.util.Date;
+import java.util.Calendar;
+
+enum Nationality {
     FRANCE , GERMANY , ITALIAN
 }
 
+
 public class person {
    private String name;
-   private int age;
+   private Date birthDate ;
    private String gender;
    private float height;
    private float weight;
    private Nationality nationality;
 
 
-   public person (String name , int age , String gender , float height , float weight, Nationality nationality ) {
+public int calculateAge (Date birthDate){
+    Calendar birth = Calendar.getInstance();
+    birth.setTime(birthDate);
+    Calendar today = Calendar.getInstance();
+
+    int age = today.get(Calendar.YEAR) - birth.get(Calendar.YEAR);
+
+    if (today.get(Calendar.MONTH) < birth.get(Calendar.MONTH) ||
+            (today.get(Calendar.MONTH) == birth.get(Calendar.MONTH) && today.get(Calendar.DAY_OF_MONTH) < birth.get(Calendar.DAY_OF_MONTH))) {
+        age--;
+    }
+
+    return age;
+
+}
+
+   public person (String name , Date birthDate , String gender , float height , float weight, Nationality nationality ) {
        this.name=name;
-       this.age=age;
+       this.birthDate= birthDate ;
        this.gender = gender ;
        this.height = height;
        this.weight = weight;
@@ -24,13 +44,24 @@ public class person {
 
    public void displayInfo(){
         System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
+        System.out.println("Age: "+ calculateAge(birthDate) +"years");
         System.out.println("Gender: " + gender);
         System.out.println("Height: " + height + " m");
         System.out.println("Weight: " + weight + " KG");
         System.out.println("Nationality: " + nationality);
 
    }
+
+  public static void main (String [] args){
+
+      Calendar calendar = Calendar.getInstance();
+      calendar.set(2000, Calendar.MARCH, 15);
+      Date birthDate = calendar.getTime();
+
+      person person = new person("Jean Dupont", birthDate, "Male", 1.75f, 70.0f, Nationality.FRANCE);
+
+      person.displayInfo();
+  }
 }
 
 
